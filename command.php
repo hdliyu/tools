70c9d43cf5ebb059a4f4a14c20d635da24150032
狂空怒 <278746453@qq.com>
2021-10-11 09:21:54 +0800

    Update command.php

--- command.php
+++ command.php
@@ -1,4 +1,9 @@
+#!/usr/bin/env php
 <?php
+/**
+ * 【用法】
+ *  php command.php 路径 【preview|html|text】
+ */
 use Hdliyu\Tools\fetch\FetchFacade;
 
 require "vendor/autoload.php";
@@ -12,9 +17,14 @@ $item = <<<item
 item;
 $path = $argv[1]??'';
 $type = $argv[2]??'';
-if(empty($path)) throw new Exception('缺少参数1：目录');
-if(!is_dir($path)) throw new Exception('目录不存在');
-if(empty($type)) throw new Exception('缺少参数2：类型，支持：【preview html text】');
-if(!in_array($type,['preview','html','text'])) throw new Exception('参数2错误，仅支持：【preview html text】');
-$action = $type == 'preview' ?'makePreview':($type=='html'?'makeHtml':'makeText');
-echo file_put_contents('~runtime.html',FetchFacade::path($path)->$action())?'【~runtime.html】生成成功':'生成失败';
+try{
+    if(empty($path)) throw new Exception('错误：请传入目录参数');
+    if(empty($type)) throw new Exception('错误：请传入类型参数');
+    if(!is_dir($path)) throw new Exception('目录不存在');
+    if(!in_array($type,['preview','html','text'])) throw new Exception('错误：类型仅支持：preview html text');
+    $action = $type == 'preview' ?'makePreview':($type=='html'?'makeHtml':'makeText');
+    echo file_put_contents('~runtime.html',FetchFacade::path($path)->$action())?'【~runtime.html】生成成功':'生成失败';
+}catch(Exception $e){
+    echo $e->getMessage();
+}
+
