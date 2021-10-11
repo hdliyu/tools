b67141081fb308e5961e9836a90d1fc6c463850e
狂空怒 <278746453@qq.com>
2021-10-11 16:07:17 +0800

    Update command.php

--- command.php
+++ command.php
@@ -8,11 +8,24 @@ use Hdliyu\Tools\fetch\FetchFacade;
 
 require "vendor/autoload.php";
 $base = <<<base
-
+<!-- #%s START 【%s】 -->
+<div class="flexW">
+<div class="proSlick">
+   %s
+</div>
+<div class="textW">
+    <h3 class="tit">%s</h3>
+    <p class="des">%s</p>
+</div>
+</div>
+<!-- #%s END 【%s】 -->
 
 base;
 
 $item = <<<item
+<div class="item">
+<img src="%s" alt="">
+</div>
 
 item;
 $path = $argv[1]??'';
@@ -23,7 +36,7 @@ try{
     if(!is_dir($path)) throw new Exception('目录不存在');
     if(!in_array($type,['preview','html','text'])) throw new Exception('错误：类型仅支持：preview html text');
     $action = $type == 'preview' ?'makePreview':($type=='html'?'makeHtml':'makeText');
-    $content = FetchFacade::path($path)->$action();
+    $content = FetchFacade::path($path)->base($base)->item($item)->$action();
     if(strlen($content)==0) throw new Exception('错误：生成失败');
     if(!file_put_contents('~runtime.html',$content)) throw new Exception('错误：文件写入失败');
     echo '【~runtime.html】生成成功';
